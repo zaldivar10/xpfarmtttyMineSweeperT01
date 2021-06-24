fcedacb06e34430efd0db82b02af166736860649
Carlos Alberto Zaldivar Perez <carlosalbertozaldivarperez@MacBook-Pro-de-Carlos-2.local>
2021-06-24 12:44:15 -0500

    feat: initial commit

---
+++ src/xpfarmtttyMineSweeperT01.js
@@ -0,0 +1,5 @@
+function xpfarmtttyMineSweeperT01 () {
+    
+}
+
+module.exports = xpfarmtttyMineSweeperT01
---
+++ test/xpfarmtttyMineSweeperT01.test.js
@@ -0,0 +1,7 @@
+const xpfarmtttyMineSweeperT01 = require('../src/xpfarmtttyMineSweeperT01.js');
+
+describe('Infrastructure Test', () => {
+    it('Canary Test', () => {      
+        expect(true).toBe(true)
+    })  
+});
