abe058769877979146317da25a710deeb6c330a2
Carlos Alberto Zaldivar Perez <carlosalbertozaldivarperez@MacBook-Pro-de-Carlos-2.local>
2021-06-24 13:11:20 -0500

    feat: adding UAT scenarios 1, 2 and 3

--- src/xpfarmtttyMineSweeperT01.js
+++ src/xpfarmtttyMineSweeperT01.js
@@ -1,5 +1,60 @@
-function xpfarmtttyMineSweeperT01 () {
-    
+function mineSweeper01() {
+    let gameMessage = "BOOM! – Game Over."
+
+    return gameMessage;
+} 
+function captureSquareSelection(squareSelection) {
+    const MAP_SQUARE_SELECTION_TO_BOARD = {
+          '0;0' : 36,
+          '0;1' : 38,
+          '0;2' : 40,
+          '1;0' : 22,
+          '1;1' : 24,
+          '1;2' : 26,
+          '2;0' : 8,
+          '2;1' : 10,
+          '2;2' : 12,
+        };
+
+    return MAP_SQUARE_SELECTION_TO_BOARD[squareSelection];
 }
 
-module.exports = xpfarmtttyMineSweeperT01
+function displaySquareValue(gameBoard, squareSelection) {
+    let squarePosition = captureSquareSelection(squareSelection);
+    const NUMBER_OF_BOMBS = ["1", "2", "3"];
+
+    if (NUMBER_OF_BOMBS.includes(gameBoard[squarePosition]))
+        console.log(gameBoard[squarePosition]," bombs around your square.")
+    return gameBoard[squarePosition];
+}
+
+function createGameBoard() {
+    let gameBoard = ['+','-','+','-','+','-','+',
+    '|',' ','|',' ','|',' ','|',
+    '+','-','+','-','+','-','+',
+    '|',' ','|',' ','|',' ','|',
+    '+','-','+','-','+','-','+', 
+    '|',' ','|',' ','|',' ','|',
+    '+','-','+','-','+','-','+'];
+    console.log(gameBoard, "[Sandbox 3x3] Game created");
+    return gameBoard;
+}
+
+function isGameOver(gameBoard, squareSelection){
+    let gameMessage = "BOOM! – Game Over."
+
+    if (displaySquareValue(gameBoard, squareSelection) == "X"){
+        console.log(gameMessage)
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+module.exports.createGameBoard = createGameBoard
+module.exports.mineSweeper01 = mineSweeper01
+module.exports.captureSquareSelection = captureSquareSelection
+module.exports.displaySquareValue = displaySquareValue
+module.exports.isGameOver = isGameOver
--- test/xpfarmtttyMineSweeperT01.test.js
+++ test/xpfarmtttyMineSweeperT01.test.js
@@ -1,7 +1,95 @@
-const xpfarmtttyMineSweeperT01 = require('../src/xpfarmtttyMineSweeperT01.js');
+const mineSweeper01 = require('../src/xpfarmtttyMineSweeperT01.js').mineSweeper01;
+const createGameBoard = require('../src/xpfarmtttyMineSweeperT01.js').createGameBoard;
+const captureSquareSelection = require('../src/xpfarmtttyMineSweeperT01.js').captureSquareSelection;
+const displaySquareValue = require('../src/xpfarmtttyMineSweeperT01.js').displaySquareValue;
+const isGameOver = require('../src/xpfarmtttyMineSweeperT01.js').isGameOver;
 
-describe('Infrastructure Test', () => {
-    it('Canary Test', () => {      
-        expect(true).toBe(true)
+describe('UAT Scenario 1: Game Board Creation', () => {
+    it('Game Board 3x3 is properly created', () => {
+        let gameBoard = ['+','-','+','-','+','-','+',
+            '|',' ','|',' ','|',' ','|',
+            '+','-','+','-','+','-','+',
+            '|',' ','|',' ','|',' ','|',
+            '+','-','+','-','+','-','+', 
+            '|',' ','|',' ','|',' ','|',
+            '+','-','+','-','+','-','+'];
+
+        expect(createGameBoard()).toEqual(gameBoard);
     })  
 });
+
+describe('UAT Scenario 2: Game Over', function() {
+    it('Game is Over when Stepping on a bomb on 1;1', () => {
+        let squareSelection = "1;1";
+        let gameMessage = "BOOM! – Game Over."
+
+        expect(mineSweeper01()).toEqual(gameMessage);
+    })
+
+})
+
+describe('Unit Tests', function() {
+    it('Capture square selection "1;1" = 24', () => {
+        let squareSelection = "1;1";
+        let squarePosition = 24;
+
+        expect(captureSquareSelection(squareSelection)).toEqual(squarePosition);
+    })
+    it('Display square value', () => {
+        let gameBoard = ['+','-','+','-','+','-','+',
+        '|',' ','|',' ','|',' ','|',
+        '+','-','+','-','+','-','+',
+        '|',' ','|','X','|',' ','|',
+        '+','-','+','-','+','-','+', 
+        '|',' ','|',' ','|',' ','|',
+        '+','-','+','-','+','-','+'];
+        let squareSelection = "1;1";
+        let squareValue = "X";
+
+        expect(displaySquareValue(gameBoard, squareSelection)).toEqual(squareValue);
+    })
+    it('Check for Game Over using selection vs bomb position in board, square contains a bomb', () => {
+        let gameBoard = ['+','-','+','-','+','-','+',
+        '|',' ','|','X','|',' ','|',
+        '+','-','+','-','+','-','+',
+        '|',' ','|',' ','|',' ','|',
+        '+','-','+','-','+','-','+', 
+        '|',' ','|',' ','|',' ','|',
+        '+','-','+','-','+','-','+'];
+        let squareSelection = "2;1";
+        let gameOver = true;
+
+        expect(isGameOver(gameBoard, squareSelection)).toEqual(gameOver);
+    })
+    it('Check for Game Over using selection vs bomb position in board, square does not contain a bomb', () => {
+        let gameBoard = ['+','-','+','-','+','-','+',
+        '|',' ','|','X','|',' ','|',
+        '+','-','+','-','+','-','+',
+        '|',' ','|',' ','|',' ','|',
+        '+','-','+','-','+','-','+', 
+        '|',' ','|',' ','|',' ','|',
+        '+','-','+','-','+','-','+'];
+        let squareSelection = "2;2";
+        let gameOver = false;
+
+        expect(isGameOver(gameBoard, squareSelection)).toEqual(gameOver);
+    })
+
+})
+
+describe('UAT Scenario 3: Clean square 0;0 and get the number of bombs around', function() {
+    it('Clean square 0;0 and get the number of bombs (3) around', () => {
+        let gameBoard = ['+','-','+','-','+','-','+',
+        '|','2','|','2','|','1','|',
+        '+','-','+','-','+','-','+',
+        '|','X','|','X','|','2','|',
+        '+','-','+','-','+','-','+', 
+        '|','3','|','X','|','2','|',
+        '+','-','+','-','+','-','+'];
+        let squareSelection = "0;0";
+        let numberOfBombsAround = "3";
+
+        expect(displaySquareValue(gameBoard, squareSelection)).toEqual(numberOfBombsAround);
+    })
+
+})
